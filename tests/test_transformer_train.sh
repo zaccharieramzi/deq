3d01f9c5cb613430f4ecc3dd68e4890edbd1e372
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-24 23:23:06 +0200

    updated the transformer test, adding the data download

--- tests/test_transformer_train.sh
+++ tests/test_transformer_train.sh
@@ -2,39 +2,35 @@
 
 cd deq/deq_sequence
 
+bash deq/deq_sequence/get_data.sh
+
 python train_transformer.py \
-        --cuda \
         --data ./data/wikitext-103/ \
         --dataset wt103 \
         --adaptive \
         --div_val 4 \
         --n_layer 2 \
-        --eval_n_layer 24 \
-        --d_embed 700 \
-        --d_model 700 \
-        --n_head 10 \
-        --d_head 70 \
-        --d_inner 48000 \
+        --eval_n_layer 2 \
+        --d_embed 10 \
+        --d_model 10 \
+        --n_head 2 \
+        --d_head 10 \
+        --d_inner 100 \
         --dropout 0.05 \
         --dropatt 0.0 \
         --optim Adam \
         --lr 0.00025 \
-        --warmup_step 16000 \
-        --pretrain_steps 32000 \
-        --eval-interval 5000 \
-        --max_step 300000 \
-        --tgt_len 150 \
-        --mem_len 150 \
-        --eval_tgt_len 150 \
+        --warmup_step 1 \
+        --pretrain_steps 2 \
+        --eval-interval 2 \
+        --max_step 4 \
+        --tgt_len 20 \
+        --mem_len 20 \
+        --eval_tgt_len 20 \
         --wnorm \
         --f_solver anderson \
         --b_solver broyden \
         --stop_mode rel \
-        --f_thres 30 \
-        --b_thres 35 \
-        --jac_loss_weight 0.0 \
-        --jac_loss_freq 0.0 \
-        --jac_incremental 0 \
-        --batch_size 56 \
-        --gpu0_bsz 14 \
-        --multi_gpu \
+        --f_thres 5 \
+        --b_thres 5 \
+        --batch_size 56

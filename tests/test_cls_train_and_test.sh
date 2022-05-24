9bd01d5d29c22d0babc396d00534900702f42add
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-24 23:03:22 +0200

    added a valid test in cls test

--- tests/test_cls_train_and_test.sh
+++ tests/test_cls_train_and_test.sh
@@ -5,4 +5,5 @@ cd deq/mdeq_vision
 tiny_args="--percent=0.0035 TRAIN.END_EPOCH 2 TRAIN.PRETRAIN_STEPS 1 DEQ.F_THRES 5 DEQ.B_THRES 5 MODEL.NUM_LAYERS 2"
 
 python tools/cls_train.py --cfg experiments/cifar/cls_mdeq_TINY.yaml $tiny_args
+python tools/cls_valid.py --cfg experiments/cifar/cls_mdeq_TINY.yaml $tiny_args
 python tools/cls_train.py --cfg experiments/cifar/cls_mdeq_LARGE_reg.yaml $tiny_args

9c9dd4df14d3c2686dfff3da4e2e7b4a1960216d
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-24 21:00:14 +0200

    corrected model pretrained path for no pretraining

--- tests/test_seg_train.sh
+++ tests/test_seg_train.sh
@@ -2,6 +2,6 @@
 
 cd deq/mdeq_vision
 
-tiny_args="--percent=0.0035 MODEL.PRETRAINED None TRAIN.END_EPOCH 2 TRAIN.PRETRAIN_STEPS 1 DEQ.F_THRES 5 DEQ.B_THRES 5 MODEL.NUM_LAYERS 2"
+tiny_args="--percent=0.0035 MODEL.PRETRAINED no_pretraining TRAIN.END_EPOCH 2 TRAIN.PRETRAIN_STEPS 1 DEQ.F_THRES 5 DEQ.B_THRES 5 MODEL.NUM_LAYERS 2"
 
 python tools/seg_train.py --cfg experiments/cifar/seg_mdeq_SMALL.yaml $tiny_args

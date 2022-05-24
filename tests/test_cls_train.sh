819640ceb2aaec49c3dee2aecb7c827e94d4f7ab
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-24 19:58:34 +0200

    made sure to have the correct args in cls train

--- tests/test_cls_train.sh
+++ tests/test_cls_train.sh
@@ -2,7 +2,7 @@
 
 cd deq/mdeq_vision
 
-tiny_args="--percent 0.01 --opts END_EPOCH 2 PRETRAIN_STEPS 1"
+tiny_args="--percent=0.01 TRAIN.END_EPOCH 2 TRAIN.PRETRAIN_STEPS 1"
 
 python tools/cls_train.py --cfg experiments/cifar/cls_mdeq_TINY.yaml $tiny_args
 python tools/cls_train.py --cfg experiments/cifar/cls_mdeq_LARGE_reg.yaml $tiny_args

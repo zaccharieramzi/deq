2648c95e0309777a4859b8337f7584f286424dc5
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-25 14:49:03 +0200

    removed large test in cls train test as already done in pytest

--- tests/test_cls_train_and_test.sh
+++ tests/test_cls_train_and_test.sh
@@ -7,4 +7,3 @@ tiny_args="--percent=0.0035 TRAIN.END_EPOCH 2 TRAIN.PRETRAIN_STEPS 1 DEQ.F_THRES
 
 python tools/cls_train.py --cfg experiments/cifar/cls_mdeq_TINY.yaml $tiny_args
 python tools/cls_valid.py --cfg experiments/cifar/cls_mdeq_TINY.yaml $tiny_args
-python tools/cls_train.py --cfg experiments/cifar/cls_mdeq_LARGE_reg.yaml $tiny_args

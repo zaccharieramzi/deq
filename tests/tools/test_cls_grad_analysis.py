8a9ca5073a0e17fcde25a73833fd344f9c17e135
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-06-01 17:31:25 +0200

    added a test for grad analysis

---
+++ tests/tools/test_cls_grad_analysis.py
@@ -0,0 +1,40 @@
+import pytest
+from unittest.mock import patch
+
+from deq.mdeq_vision.tools.cls_train import main
+from deq.mdeq_vision.tools.cls_grad_analysis import main as cls_grad_analysis_main
+
+
+@pytest.mark.parametrize("config", [
+    "TINY",
+    "LARGE_reg",
+])
+def test_cls_grad(config):
+    args = [
+        "main",
+        "--save_at",
+        "1",
+        "--cfg",
+        f"deq/mdeq_vision/experiments/cifar/cls_mdeq_{config}.yaml",
+        "--percent",
+        "0.0035",
+        "TRAIN.END_EPOCH",
+        "2",
+        "TRAIN.PRETRAIN_STEPS",
+        "1",
+        "DEQ.F_THRES",
+        "5",
+        "DEQ.B_THRES",
+        "5",
+        "MODEL.NUM_LAYERS",
+        "2",
+    ]
+    with patch("sys.argv", args):
+        main()
+
+    args += [
+        "TRAIN.BEGIN_EPOCH",
+        "1",
+    ]
+    with patch("sys.argv", args):
+        cls_grad_analysis_main()

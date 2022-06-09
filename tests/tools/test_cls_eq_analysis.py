6a4afaa73001bc7c98db105864bbfdbcb22c68ba
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-06-09 14:15:30 +0200

    added a test for eq analysis w broyden matrices

--- tests/tools/test_cls_eq_analysis.py
+++ tests/tools/test_cls_eq_analysis.py
@@ -39,3 +39,36 @@ def test_cls_eq(config):
     args[1] = "--n_images"
     with patch("sys.argv", args):
         cls_eq_analysis_main()
+
+
+def test_cls_eq_broyden_matrices(config):
+    args = [
+        "main",
+        "--save_at",
+        "1",
+        "--cfg",
+        f"deq/mdeq_vision/experiments/cifar/cls_mdeq_{config}.yaml",
+        "--percent",
+        "0.0035",
+        "TRAIN.END_EPOCH",
+        "2",
+        "TRAIN.PRETRAIN_STEPS",
+        "1",
+        "DEQ.F_THRES",
+        "2",
+        "DEQ.B_THRES",
+        "2",
+        "MODEL.NUM_LAYERS",
+        "2",
+    ]
+    with patch("sys.argv", args):
+        main()
+
+    args += [
+        "TRAIN.BEGIN_EPOCH",
+        "1",
+    ]
+    args[1] = "--n_images"
+    args.insert(1, "--broyden_matrices")
+    with patch("sys.argv", args):
+        cls_eq_analysis_main()

7a34c4951fed48dbac0bf5b84c4e6714e7ab45be
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-07-07 15:57:39 +0200

    added warm init dir to test grad analysis

--- tests/tools/test_cls_grad_analysis.py
+++ tests/tools/test_cls_grad_analysis.py
@@ -12,29 +12,22 @@ from deq.mdeq_vision.tools.cls_grad_analysis import main as cls_grad_analysis_ma
 def test_cls_grad(config):
     args = [
         "main",
-        "--save_at",
-        "1",
+        "--save_at", "1",
         "--cfg",
         f"deq/mdeq_vision/experiments/cifar/cls_mdeq_{config}.yaml",
-        "--percent",
-        "0.0035",
-        "TRAIN.END_EPOCH",
-        "2",
-        "TRAIN.PRETRAIN_STEPS",
-        "1",
-        "DEQ.F_THRES",
-        "2",
-        "DEQ.B_THRES",
-        "2",
-        "MODEL.NUM_LAYERS",
-        "2",
+        "--percent", "0.0035",
+        "TRAIN.END_EPOCH", "2",
+        "TRAIN.PRETRAIN_STEPS", "1",
+        "DEQ.F_THRES", "2",
+        "DEQ.B_THRES", "2",
+        "MODEL.NUM_LAYERS", "2",
     ]
     with patch("sys.argv", args):
         main()
 
     args += [
-        "TRAIN.BEGIN_EPOCH",
-        "1",
+        "TRAIN.BEGIN_EPOCH", "1",
+        "TRAIN.WARM_INIT_DIR", "./",
     ]
     args[1] = "--n_images"
     with patch("sys.argv", args):

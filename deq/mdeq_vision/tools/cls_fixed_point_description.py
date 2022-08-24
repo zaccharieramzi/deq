c4321ab6bff51811bd06e7a98e0602b3c67660cb
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-08-24 15:28:41 +0200

    added a script to find the norm of the fixed point

---
+++ deq/mdeq_vision/tools/cls_fixed_point_description.py
@@ -0,0 +1,235 @@
+# Modified based on the HRNet repo.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+from pathlib import Path
+import pprint
+import time
+
+import numpy as np
+import pandas as pd
+from termcolor import colored
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+
+from deq.lib.optimizations import VariationalHidDropout2d
+from deq.mdeq_vision.lib import models  # noqa F401
+from deq.mdeq_vision.lib.config import config
+from deq.mdeq_vision.lib.config import update_config
+from deq.mdeq_vision.lib.core.cls_function import train
+from deq.mdeq_vision.lib.utils.utils import get_optimizer
+from deq.mdeq_vision.lib.utils.utils import create_logger
+
+
+def set_dropout_modules_active(model):
+    for m in model.modules():
+        if isinstance(m, VariationalHidDropout2d):
+            m.train()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train classification network')
+
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        required=True,
+                        type=str)
+    parser.add_argument('--modelDir',
+                        help='model directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--logDir',
+                        help='log directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--dataDir',
+                        help='data directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--testModel',
+                        help='testModel',
+                        type=str,
+                        default='')
+    parser.add_argument('--percent',
+                        help='percentage of training data to use',
+                        type=float,
+                        default=1.0)
+    parser.add_argument('--save_at',
+                        help='''save checkpoint at certain epochs,
+                        space-separated''',
+                        type=int,
+                        nargs='+')
+    parser.add_argument('--dropout_eval',
+                        help='whether to use dropout during the evaluation',
+                        action='store_true')
+    parser.add_argument('--broyden_matrices',
+                        help='whether to use broyden matrices when doing warm init',
+                        action='store_true')
+    parser.add_argument('--n_images',
+                        help='number of images to use for evaluation',
+                        type=int,
+                        default=10)
+    parser.add_argument('--results_name',
+                        help='name of the results file',
+                        type=str,
+                        default='eq_init_results.csv')
+    parser.add_argument('--seed',
+                        help='random seed',
+                        type=int,
+                        default=None)
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+    update_config(config, args)
+
+    return args
+
+
+def main():
+    """
+    Set the --percent to make the duration of training vary.
+    Set the --dropout_eval to use dropout during the evaluation.
+    Set the --broyden_matrices to use broyden matrices when doing warm init.
+    Set the TRAIN.BEGIN_EPOCH for the checkpoint
+    """
+    args = parse_args()
+    seed = args.seed
+    seeding = seed is not None
+    torch.manual_seed(seed if seeding else 42)
+    try:
+        torch.multiprocessing.set_start_method('spawn')
+    except RuntimeError:
+        pass
+    if torch.cuda.is_available():
+        print(colored("Setting default tensor type to cuda.FloatTensor", "cyan"))
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        device_str = 'cuda'
+    else:
+        device_str = 'cpu'
+
+    logger, final_output_dir, tb_log_dir = create_logger(
+        config, args.cfg, 'train')
+
+    logger.info(pprint.pformat(args))
+    logger.info(pprint.pformat(config))
+
+    # cudnn related setting
+    if not seeding:
+        cudnn.benchmark = config.CUDNN.BENCHMARK
+        torch.backends.cudnn.deterministic = config.CUDNN.DETERMINISTIC
+    else:
+        cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.enabled = config.CUDNN.ENABLED
+
+    model = eval('models.'+config.MODEL.NAME+'.get_cls_net')(config)
+    if torch.cuda.is_available():
+        model = model.cuda()
+
+    if torch.cuda.is_available():
+        gpus = list(config.GPUS)
+        model = nn.DataParallel(model, device_ids=gpus).cuda()
+    print("Finished constructing model!")
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss()
+    if torch.cuda.is_available():
+        criterion = criterion.cuda()
+
+    last_epoch = config.TRAIN.BEGIN_EPOCH
+    checkpoint_name = 'checkpoint'
+    if seeding:
+        checkpoint_name += f'_seed{seed}'
+    model_state_file = os.path.join(
+        final_output_dir,
+        f'{checkpoint_name}_{last_epoch}.pth.tar',
+    )
+    if torch.cuda.is_available():
+        checkpoint = torch.load(model_state_file)
+        model.module.load_state_dict(checkpoint['state_dict'])
+    else:
+        checkpoint = torch.load(model_state_file, map_location='cpu')
+        model.load_state_dict(checkpoint['state_dict'])
+
+    logger.info("=> loaded checkpoint (epoch {})".format(checkpoint['epoch']))
+
+    # Data loading code
+    dataset_name = config.DATASET.DATASET
+
+    if dataset_name == 'imagenet':
+        traindir = os.path.join(config.DATASET.ROOT+'/images', config.DATASET.TRAIN_SET)
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        augment_list = [
+            transforms.RandomResizedCrop(config.MODEL.IMAGE_SIZE[0]),
+            transforms.RandomHorizontalFlip(),
+        ]
+        transform_train = transforms.Compose(augment_list + [
+            transforms.ToTensor(),
+            normalize,
+        ])
+        aug_train_dataset = datasets.ImageFolder(traindir, transform_train)
+    else:
+        assert dataset_name == "cifar10", "Only CIFAR-10 and ImageNet are supported at this phase"
+        classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')  # For reference
+
+        normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        augment_list = [transforms.RandomCrop(32, padding=4), transforms.RandomHorizontalFlip()]
+        transform_train = transforms.Compose(augment_list + [
+            transforms.ToTensor(),
+            normalize,
+        ])
+        aug_train_dataset = datasets.CIFAR10(
+            root=f'{config.DATASET.ROOT}',
+            train=True,
+            download=True,
+            transform=transform_train,
+        )
+
+    batch_size = config.TRAIN.BATCH_SIZE_PER_GPU
+    if torch.cuda.is_available():
+        batch_size = batch_size * len(config.GPUS)
+
+    # Evaluating convergence before training
+    model.eval()
+    if args.dropout_eval:
+        set_dropout_modules_active(model)
+    image_indices = np.random.choice(
+        len(aug_train_dataset),
+        args.n_images,
+        replace=False,
+    )
+    fn = model.module._forward if torch.cuda.is_available() else model._forward
+    fixed_point_norms = []
+    for image_index in image_indices:
+        image, _ = aug_train_dataset[image_index]
+        image = image.unsqueeze(0)
+        if torch.cuda.is_available():
+            image = image.cuda()
+        # pot in kwargs we can have: f_thres, b_thres, lim_mem
+        *_, new_inits = fn(image, train_step=-1, return_init=True)
+        fixed_point = new_inits[0]
+        # append to fixed_point_norms the norm of each sample in
+        # fixed_point (batch_size x ...)
+        norms = fixed_point.norm(dim=(1, 2, 3))
+        fixed_point_norms.append(norms[0].cpu().numpy().item())
+    print(fixed_point_norms)
+    return fixed_point_norms
+
+
+if __name__ == '__main__':
+    main()

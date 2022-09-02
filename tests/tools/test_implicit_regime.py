4a9213a1322f6bdb93129b96a299f3b51fb504ae
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-09-02 11:54:44 +0200

    cleaned up implicit regime test and added a sanity check

--- tests/tools/test_implicit_regime.py
+++ tests/tools/test_implicit_regime.py
@@ -1,5 +1,5 @@
-import os
-
+import pandas as pd
+import numpy as np
 import pytest
 from unittest.mock import patch
 
@@ -12,7 +12,7 @@ from deq.mdeq_vision.tools.cls_implicit_regime_identification import (
 @pytest.mark.parametrize("config", [
     "TINY",
 ])
-def test_cls_implicit_regime(config):
+def test_cls_implicit_regime(config, clean_up_files):
     args = [
         "main",
         "--save_at",
@@ -51,8 +51,48 @@ def test_cls_implicit_regime(config):
     with patch("sys.argv", args + opts):
         implicit_regime_main()
 
-    # clean up all *.pt files in the ./ dir
-    for f in os.listdir("./"):
-        if f.endswith(".pt"):
-            os.remove(f)
+@pytest.mark.parametrize("config", [
+    "TINY",
+])
+def test_cls_implicit_regime_sanity_check(config, clean_up_files):
+    args = [
+        "main",
+        "--save_at",
+        "1",
+        "--cfg",
+        f"deq/mdeq_vision/experiments/cifar/cls_mdeq_{config}.yaml",
+        "--percent",
+        "0.0035",
+    ]
+    opts = [
+        "TRAIN.END_EPOCH",
+        "2",
+        "TRAIN.PRETRAIN_STEPS",
+        "1",
+        "DEQ.F_THRES",
+        "2",
+        "DEQ.B_THRES",
+        "2",
+        "MODEL.NUM_LAYERS",
+        "2",
+    ]
+    with patch("sys.argv", args + opts):
+        main()
 
+    args[1] = "--n_batches"
+    args.extend([
+        "--b_thres_range", "100", "101", "1",
+        "--f_thres_range", "100", "101", "1",
+    ])
+    opts += [
+        "TEST.MODEL_FILE", "output/cifar10/cls_mdeq_TINY/checkpoint_1.pth.tar",
+        "TRAIN.BEGIN_EPOCH", "1",
+        "TRAIN.WARM_INIT_DIR", "./",
+        "TRAIN.BATCH_SIZE_PER_GPU", "2",
+    ]
+    with patch("sys.argv", args + opts):
+        implicit_regime_main()
+
+    df_results = pd.read_csv("implicit_regime_identification.csv")
+    diff_norm = df_results['grad_diff_norm']
+    np.testing.assert_almost_equal(diff_norm, np.zeros(len(diff_norm)))

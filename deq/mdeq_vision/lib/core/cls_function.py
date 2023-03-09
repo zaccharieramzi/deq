9dfec45649318b28cf50ee400ea835286c6159d2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2023-03-09 16:04:48 +0100

    corrected way to agg cvg vars

--- deq/mdeq_vision/lib/core/cls_function.py
+++ deq/mdeq_vision/lib/core/cls_function.py
@@ -278,8 +278,8 @@ def validate(config, val_loader, model, criterion, lr_scheduler, epoch, output_d
             top1.update(prec1[0], input.size(0))
             top5.update(prec5[0], input.size(0))
             if return_convergence:
-                convergence_rel.update(result_fw['rel_trace'].item(), input.size(0))
-                convergence_abs.update(result_fw['abs_trace'].item(), input.size(0))
+                convergence_rel.update(result_fw['rel_trace'].mean(), input.size(0))
+                convergence_abs.update(result_fw['abs_trace'].mean(), input.size(0))
 
             if spectral_radius_mode:
                 sradius = sradius.mean()
